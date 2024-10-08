c84109d7bc1e81ec8c72c8e84ee02952f9696661
Cooper Doucette <coopdouce01@gmail.com>
2024-10-07 20:52:37 -0500

    Updated Main Files, Still Unfinished

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { FaGithub, FaStackOverflow } from "react-icons/fa";
+import { SiIndeed } from "react-icons/si";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer-container">
+      <p>© 2024 Cooper Doucette</p>
+      <div>
+        <a className="social-icons"
+            href="https://profile.indeed.com/p/cooperd-5f1spgk"
+        >
+            <SaIndeed style={{ fontSize: "30px" }} />
+        </a>
+        <a className="social-icons"
+            href="https://github.com/CoopDouce"
+        >
+            <FaGithub style={{ fontSize: "30px" }} />
+        </a>
+        <a className="social-icons"
+            href="https://stackoverflow.com/users/27638236/cooper-doucette"
+        >
+            <FaStackOverflow style={{ fontSize: "30px" }} />
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/Components/Nav/Nav.jsx
@@ -0,0 +1,35 @@
+import { Link, useLocation } from 'react-router-dom';
+import './Nav.css';
+
+function Nav() {
+  const location = useLocation();
+
+  return (
+    <nav className="nav-container">
+      <ul className="nav-list">
+        <li className="nav-item">
+          <Link to="/" className={currentPage === "/" ? "activeLink" : "link"}>
+            About Me
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/Portfolio" className={currentPage === "/portfolio" ? "activeLink" : "link"}>
+            Portfolio
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/Contact" className={currentPage === "/contact" ? "activeLink" : "link"}>
+            Contact
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/Resume" className={currentPage === "/contact" ? "activeLink" : "link"}>
+            resume
+          </Link>
+        </li>
+        </ul>
+    </nav>
+    );
+}
+
+export default Nav;
